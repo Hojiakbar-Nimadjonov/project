80fb36474e98a9b10d8b817003620691d76c4d73
Hojiakbar-Nimadjonov <hadjihima77@gmail.com>
2025-03-12 23:25:59 +0500

    Add files via upload

---
+++ SQLQuery3.sql
@@ -0,0 +1,12 @@
+SELECT * FROM ConstraintDemo2;
+
+
+Create table category (
+id int,
+[name] varchar(50),
+[desc] varchar(50)
+)
+
+insert into category values
+(1, 'horror', null),4
+(2, 'Comedy', null)
